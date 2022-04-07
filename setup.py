fd08b8de90b4bcd087349d4c15930ac9e39f0ddd
David Hurley <drhurley94@gmail.com>
2022-04-06 20:11:33 -0400

    add original author to setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="pritunl-api",
     version="0.0.1",
-    author="Dave Hurley",
-    author_email="drhurley94@gmail.com",
+    author="ijat, dhurley94",
+    author_email="contact@ijat.my, drhurley94@gmail.com",
     description="A simple Pritunl API wrapper. Forked from https://github.com/ijat/pritunl-api-python",
     long_description=long_description,
     long_description_content_type="text/markdown",
